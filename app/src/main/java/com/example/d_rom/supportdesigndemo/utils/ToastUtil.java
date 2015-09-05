b0b4c8df385ca807b3f2c207fef6d0ce4abf2068
Yaroslav-Syrota <syrota.yaroslav@gmail.com>
2015-09-05 15:37:54 +0300

    add method addLongToast

--- app/src/main/java/com/example/d_rom/supportdesigndemo/utils/ToastUtil.java
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/utils/ToastUtil.java
@@ -1,7 +1,13 @@
 package com.example.d_rom.supportdesigndemo.utils;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
  * Created by Admin on 05.09.2015.
  */
 public class ToastUtil {
+    public static void addLongToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
 }
