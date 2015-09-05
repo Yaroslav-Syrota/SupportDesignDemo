95fc080386e5edfccbf7cc2d7ebbcc08e019ee12
Yaroslav-Syrota <syrota.yaroslav@gmail.com>
2015-09-05 16:00:10 +0300

    add getters and setters

--- app/src/main/java/com/example/d_rom/supportdesigndemo/entities/MyMessage.java
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/entities/MyMessage.java
@@ -4,4 +4,13 @@ package com.example.d_rom.supportdesigndemo.entities;
  * Created by Admin on 05.09.2015.
  */
 public class MyMessage {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
